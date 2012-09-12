02ab084d7b8a9c2f6080206bf45373f3c9476a7b
Gregory Jefferis <jefferis@gmail.com>
2012-09-12 22:39:49 +0100

    Add R startup file

---
+++ src/JFStartup.R
@@ -0,0 +1,12 @@
+jfconfig<-list(
+	url="http://flweb.janelia.org/",
+	srcdir=path.expand(dirname(attr(body(function() {}),'srcfile')$filename))
+)
+jfconfig$rootdir=dirname(jfconfig$srcdir)
+jfconfig$regroot=file.path(jfconfig$rootdir,'FLReg')
+jfconfig$dbdir=file.path(jfconfig$rootdir,'db')
+jfconfig$FunctionFiles=list.files(jfconfig$srcdir,patt="Functions",full=T,recurs=T)
+
+for (MyPath in jfconfig$FunctionFiles) {
+	try(source(MyPath))
+}
