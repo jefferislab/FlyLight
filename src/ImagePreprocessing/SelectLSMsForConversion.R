cde22b8ebc32da003eeea337def3804426bad2ea
Gregory Jefferis <jefferis@gmail.com>
2012-09-12 23:32:16 +0100

    add startup snippet

--- src/ImagePreprocessing/SelectLSMsForConversion.R
+++ src/ImagePreprocessing/SelectLSMsForConversion.R
@@ -1,6 +1,11 @@
 # for the time being, symlink all lsms in dump directory
 # in future consider processing VNCs/Brains separately
 
+if(!exists('jfconfig')){
+	source(path.expand("~/projects/AnalysisSuite/R/Code/Startup.R"),keep.source=T)
+	source("~/projects/JFRC/FlyLight/src/JFStartup.R",keep.source=T)
+}
+
 lsms=dir(jfconfig$dumpdir,patt="\\.lsm$",recursive=TRUE)
 
 for(lsm in lsms){
