d99074a52d36145083b0c0ce9ba207d39d80f6f3
Gregory Jefferis <jefferis@gmail.com>
2012-09-20 19:01:46 +0100

    fix spec of input files

--- src/ImagePostprocessing/CalculateBackgroundParams.R
+++ src/ImagePostprocessing/CalculateBackgroundParams.R
@@ -12,8 +12,8 @@ jfrc2csv=file.path(jfconfig$dbdir,"jfrc2_BackgroundParams.csv")
 jfrc2histo=file.path(jfconfig$regdir,"reformatted.histo")
 if(file.exists(jfrc2csv)){
 	jfrc2df=read.csv(jfrc2csv,stringsAsFactors=FALSE)
-	jfrc2df=UpdateOrCalculateBackgroundParams(,jfrc2df)
-} else jfrc2df=UpdateOrCalculateBackgroundParams(jfrc2files)
+	jfrc2df=UpdateOrCalculateBackgroundParams(jfrc2histo,jfrc2df)
+} else jfrc2df=UpdateOrCalculateBackgroundParams(jfrc2histo)
 
 if(!isTRUE(attr(jfrc2df,"changed")==FALSE))
 	write.csv(jfrc2df,file=jfrc2csv,row.names=FALSE)
