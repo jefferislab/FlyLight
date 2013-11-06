7579cbd0aae614ec96a01c6a193d249ea6e764af
Gregory Jefferis <jefferis@gmail.com>
2013-11-06 16:58:08 +0000

    Teach jfimagestem_forfile() to cope with factors

--- src/FlyLightDB/JFBaseFunctions.R
+++ src/FlyLightDB/JFBaseFunctions.R
@@ -9,6 +9,7 @@
 #' stopifnot(jfimagestem_forfile("Registration.GHT/affine/JFRC2_GMR_10A06_AE_01_08-fA01b_C100709_20100709225242046_01_ght.list")=="GMR_10A06_AE_01_08-fA01b_C100709_20100709225242046")
 jfimagestem_forfile<-function(file,checkExists=FALSE){
 	# get rid of any leading directories
+	if(is.factor(file)) file=as.character(file)
 	jfi=basename(file)
 	# GMR_9G09_AE_01_41-fA01b_C100120_20100120131426046
 	regex="GMR_[0-9]{1,2}[A-H][0-9]{2}_[A-Z]{2}_[0-9]{2}_[0-9]{2}-fA0[1-2](b|v)_C[0-9]{6}_[0-9]{17}"
